996267ff3a48ae3ae1cc65daaafa7109f98211ff
saravanan7878 <saravanancloudcomputing@gmail.com>
2017-05-07 21:24:05 -0700

    file updated

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ public static void main (String[] args )
 {
     for (int i =0; i<=9; i++)
       {
-        System.out.println("welcome to  jenkins");
+        System.out.println("welcome to  jenkins part 2");
          }
   }
 }
